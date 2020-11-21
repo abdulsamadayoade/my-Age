c7f1b0c8033c2218daba9ff98a9c9e09de5d7985
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2020-11-21 23:43:27 +0100

    Add and style the error div

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
 const form = document.getElementById('form');
 const date = new Date().getFullYear();
+const dateField = document.getElementById('date-field');
+const ageField = document.getElementById('age-field');
 
 form.addEventListener('submit', calculateAge);
 
 function calculateAge(e) {
-
-    const dateField = document.getElementById('date-field');
-    const userDate = dateField.value;
-
-    const ageField = document.getElementById('age-field');
-    ageField.value = date - userDate;
+    if (dateField.value !== '') {
+        const userDate = dateField.value;
+        ageField.value = date - userDate;
+    } else {
+        errorMsg('Please input your year of birth');
+    }
 
     e.preventDefault();
+}
+
+function errorMsg(error) {
+    // CREATE THE ERROR DIV
+    const errorDiv = document.createElement('div');
+
+    // GET ELEMENTS
+    const heading = document.querySelector('.heading');
+    const container = document.querySelector('.container');
+
+    // ADD CLASSES TO IT
+    errorDiv.className = 'alert alert-danger';
+
+    // ADD TEXTNODE
+    errorDiv.appendChild(document.createTextNode(error));
+
+    // INSERT THE ERROR DIV IN THE CONTAINER BUT BEFORE THE HEADING
+    container.insertBefore(errorDiv, heading);
+
+    // TIMEOUT
+    setTimeout(clearErrorMsg, 3000);
+}
+
+function clearErrorMsg() {
+    document.querySelector('.alert').remove();
 }
